253af584d376db27c431c209e2be63763c94c2a3
Priyanka Pazhayedath Prakasan <priyankaprakasan@gmail.com>
2026-07-06 11:45:21 +0200

    Update repo_id for HRBot upload

--- hosting/hosting.py
+++ hosting/hosting.py
@@ -4,7 +4,7 @@ HF_TOKEN = os.environ.get("HF_TOKEN", "")
 api = HfApi(token=HF_TOKEN)
 api.upload_folder(
     folder_path="deployment",     # the local folder containing your files
-    repo_id="priyankaprakasan/HRchatBot",          # the target repo
+    repo_id="priyankaprakasan/HRBot",          # the target repo
     repo_type="space",                      # dataset, model, or space
     path_in_repo="",                          # optional: subfolder path inside the repo
 )
